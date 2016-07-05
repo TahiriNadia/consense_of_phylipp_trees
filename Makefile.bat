36a65386b8fcb05f17b54f9dadb16550d09feaea
NadiaUQAM <nadia_tahiri@yahoo.fr>
2016-07-05 16:23:11 -0400

    Version 1

--- Makefile.bat
+++ Makefile.bat
@@ -1,5 +1,1 @@
-cc -O3 -fomit-frame-pointer   -c -o phylip.o phylip.c
-cc -O3 -fomit-frame-pointer   -c -o consense.o consense.c
-cc -O3 -fomit-frame-pointer -Wno-write-strings  -c -o main.o main.cpp
-cc -O3 -fomit-frame-pointer   -c -o cons.o cons.c
-cc -O3 -fomit-frame-pointer consense.o phylip.o cons.o main.o -lm -o consense
+cc -O3 -fomit-frame-pointer -Wno-write-strings  -c main.cpp -openmp -Wno-write-strings -g -o cSuperTree
